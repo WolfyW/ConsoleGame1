6f50500167706de81d6b2aa69aa61562e8c41afa
Петр Васильевич <kokarev_da@pmtt.ru>
2018-03-13 07:32:57 +0200

    Добавьте файлы проекта.

---
+++ ConsoleGame/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    class Game
+    {
+        readonly char[][] levelData = {
+            "###################################".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#                                 #".ToCharArray(),
+            "#h                                #".ToCharArray(),
+            "#################################e#".ToCharArray(),
+        };
+
+        int rowCount = 15;
+        int columnCount = 35;
+        bool isGameACtive = true;
+        public Game()
+        {
+            Initialize();
+            while (isGameACtive)
+            {
+                Render();
+                Update();
+            }
+        }
+
+        void Initialize()
+        {
+
+        }
+
+        void Render()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+
+    }
+}
